433509b7f77ebbf91e4034520f8fe97e75fb3e82
Eniola Codes <eniolacodes1@gmail.com>
2022-08-14 10:32:17 +0100

    Trial2 Edits

--- src/components/SectionComponents/HeroThreeContent.js
+++ src/components/SectionComponents/HeroThreeContent.js
@@ -1,31 +1,29 @@
-// import React from 'react';
-// import { Row, Container } from "react-bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import classes from './'
+import React from 'react';
+import classes from './HeroThreeContent.module.css'
+import HeroThreeForm from './HeroThreeForm';
 
-// const HeroThreeContent = () =>
-// {
-//     <Col
-//     lg={4}
-//     className={classes.dish_col}
-//     data-aos="fade-up"
-//     data-aos-easing="ease-out"
-//     data-aos-duration="700"
-//   >
-//     <div className={classes.dish_content}>
-//       <div className={classes.dish_image_div}>
-//         <img src={dish1} alt="Dish" />
-//       </div>
-//       <div className={classes.dish_text_div}>
-//         <p>Yam and egg sauce</p>
-//       </div>
-//       <div className={classes.dish_price_div}>
-//         <p className="my-auto">₦1200</p>
-//        <HeroThreeForm onAddToCart={onAddToCartHandler}/>
-//       </div>
-//     </div>
-//   </Col>
+const onAddToCartHandler = (amount) =>
+{
+    
+}
 
-// }
+const HeroThreeContent = (props) =>
+{
 
-// export default HeroThreeContent;
+    return (
+    <div className={classes.dish_content}>
+      <div className={classes.dish_image_div}>
+        <img src={props.src} alt="Dish"/>
+      </div>
+      <div className={classes.dish_text_div}>
+        <p>{props.name}</p>
+      </div>
+      <div className={classes.dish_price_div}>
+        <p className="my-auto">₦{props.price}</p>
+       <HeroThreeForm onAddToCart={onAddToCartHandler}/>
+      </div>
+    </div>
+)
+}
+
+export default HeroThreeContent;
--- src/components/Sections/HeroThreeSection.js
+++ src/components/Sections/HeroThreeSection.js
@@ -2,29 +2,103 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import classes from "./HeroThreeSection.module.css";
-import Input from "../Ui/Input";
-import TheButton from "../Ui/TheButton";
 import dish1 from "../../assets/image/dish1.png";
 import dish2 from "../../assets/image/dish2.png";
 import dish3 from "../../assets/image/dish3.png";
-import HeroThreeForm from "../SectionComponents/HeroThreeForm";
+import dish4 from "../../assets/image/dish4.png";
+import dish5 from "../../assets/image/dish5.png";
+import dish6 from "../../assets/image/dish6.png";
+
+import HeroThreeContent from "../SectionComponents/HeroThreeContent";
+
+const dummyList = [
+  {
+    id: "d1",
+    src: dish1,
+    name: "Yam and egg sauce",
+    price: "1500",
+  },
+
+  {
+    id: "d2",
+    src: dish2,
+    name: "Jellof rice and chicken",
+    price: " 3500",
+  },
+
+  {
+    id: "d3",
+    src: dish3,
+    name: "Porridge beans",
+    price: "2000",
+  },
+];
+
+const dummyList2 = [
+  {
+    id: "d4",
+    src: dish4,
+    name: "Semo and egusi soup",
+    price: "5000",
+  },
+
+  {
+    id: "d5",
+    src: dish5,
+    name: "Amala and ewedu soup",
+    price: "7000",
+  },
+
+  {
+    id: "d6",
+    src: dish6,
+    name: "Eba and okra soup",
+    price: "3000",
+  },
+];
 
 const HeroThreeSection = () => {
- 
-const onAddToCartHandler = (amount) =>
-{
-    
-}
+
+  const dishList1 = dummyList.map((dish) => (
+    <Col
+      lg={4}
+      className={classes.dish_col}
+      data-aos="fade-up"
+      data-aos-easing="ease-out"
+      data-aos-duration="700"
+    >
+      <HeroThreeContent
+        key={dish.id}
+        name={dish.name}
+        src={dish.src}
+        price={dish.price}
+      />
+   </Col>
+  ));
+
+  const dishList2 = dummyList2.map((dish) => (
+    <Col
+      lg={4}
+      className={classes.dish_col}
+      data-aos="fade-up"
+      data-aos-easing="ease-out"
+      data-aos-duration="700"
+    >
+      <HeroThreeContent
+        key={dish.id}
+        name={dish.name}
+        src={dish.src}
+        price={dish.price}
+      />
+   </Col>
+  ));
+
+
 
 
   return (
     <section>
       <Container>
-
-
-
-
-
         <Row
           className={`${classes.row} mx-auto`}
           bdata-aos="fade-up"
@@ -38,163 +112,14 @@ const onAddToCartHandler = (amount) =>
             </div>
           </Col>
         </Row>
+
         <Row className={classes.row_dish}>
+          {dishList1}
+          </Row>
 
-
-
-
-
-          <Col
-            lg={4}
-            className={classes.dish_col}
-            data-aos="fade-up"
-            data-aos-easing="ease-out"
-            data-aos-duration="700"
-          >
-            <div className={classes.dish_content}>
-              <div className={classes.dish_image_div}>
-                <img src={dish1} alt="Dish" />
-              </div>
-              <div className={classes.dish_text_div}>
-                <p>Yam and egg sauce</p>
-              </div>
-              <div className={classes.dish_price_div}>
-                <p className="my-auto">₦1200</p>
-               <HeroThreeForm onAddToCart={onAddToCartHandler}/>
-              </div>
-            </div>
-          </Col>
-
-
-
-
-
-          <Col
-            lg={4}
-            className={`${classes.dish_col} ${classes.dish_col_mid}`}
-            data-aos="fade-up"
-            data-aos-easing="ease-out"
-            data-aos-duration="700"
-          >
-            <div className={classes.dish_content}>
-              <div className={classes.dish_image_div}>
-                <img src={dish2} alt="Dish" />
-              </div>
-              <div className={classes.dish_text_div}>
-                <p>Yam and egg sauce</p>
-              </div>
-              <div className={classes.dish_price_div}>
-                <p className="my-auto">₦1200</p>
-                <Input />
-                <TheButton>
-                  <i className="bi bi-plus"></i> Add
-                </TheButton>
-              </div>
-            </div>
-          </Col>
-          <Col
-            lg={4}
-            className={classes.dish_col}
-            data-aos="fade-up"
-            data-aos-easing="ease-out"
-            data-aos-duration="700"
-          >
-            <div className={classes.dish_content}>
-              <div className={classes.dish_image_div}>
-                <img src={dish3} alt="Dish" />
-              </div>
-              <div className={classes.dish_text_div}>
-                <p>Yam and egg sauce</p>
-              </div>
-              <div className={classes.dish_price_div}>
-                <p className="my-auto">₦1200</p>
-                <Input />
-                <TheButton>
-                  <i className="bi bi-plus"></i> Add
-                </TheButton>
-              </div>
-            </div>
-          </Col>
-        </Row>
-
-
-
-
-
-        <Row>
-          <Col
-            lg={4}
-            className={classes.dish_col}
-            data-aos="fade-up"
-            data-aos-easing="ease-out"
-            data-aos-duration="700"
-          >
-            <div className={classes.dish_content}>
-              <div className={classes.dish_image_div}>
-                <img src={dish1} alt="Dish" />
-              </div>
-              <div className={classes.dish_text_div}>
-                <p>Yam and egg sauce</p>
-              </div>
-              <div className={classes.dish_price_div}>
-                <p className="my-auto">₦1200</p>
-                <Input />
-                <TheButton>
-                  <i className="bi bi-plus"></i> Add
-                </TheButton>
-              </div>
-            </div>
-          </Col>
-          <Col
-            lg={4}
-            className={`${classes.dish_col} ${classes.dish_col_mid}`}
-            data-aos="fade-up"
-            data-aos-easing="ease-out"
-            data-aos-duration="700"
-          >
-            <div className={classes.dish_content}>
-              <div className={classes.dish_image_div}>
-                <img src={dish2} alt="Dish" />
-              </div>
-              <div className={classes.dish_text_div}>
-                <p>Yam and egg sauce</p>
-              </div>
-              <div className={classes.dish_price_div}>
-                <p className="my-auto">₦1200</p>
-                <Input />
-                <TheButton>
-                  <i className="bi bi-plus"></i> Add
-                </TheButton>
-              </div>
-            </div>
-          </Col>
-          <Col
-            lg={4}
-            className={classes.dish_col}
-            data-aos="fade-up"
-            data-aos-easing="ease-out"
-            data-aos-duration="700"
-          >
-            <div className={classes.dish_content}>
-              <div className={classes.dish_image_div}>
-                <img src={dish3} alt="Dish" />
-              </div>
-              <div className={classes.dish_text_div}>
-                <p>Yam and egg sauce</p>
-              </div>
-              <div className={classes.dish_price_div}>
-                <p className="my-auto">₦1200</p>
-                <Input />
-                <TheButton>
-                  <i className="bi bi-plus"></i> Add
-                </TheButton>
-              </div>
-            </div>
-          </Col>
-        </Row>
-
-
-
+          <Row>
+          {dishList2}
+          </Row>
 
 
       </Container>
