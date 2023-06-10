70310ddb4b8f123dceb2bced1dfb64eea4b0ec12
Nizam <nizamy710@gmail.com>
2023-06-10 23:55:27 +0530

    added cart button for mobile

--- src/components/Header/TheNavbar.js
+++ src/components/Header/TheNavbar.js
@@ -1,96 +1,94 @@
-import React from "react";
-import NavCartButton from "./NavCartButton";
-import { Navbar, Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-scroll";
-import classes from "./TheNavbar.module.css";
-import Logo from "../../assets/Logo/Logo.svg";
+import React from 'react';
+import NavCartButton from './NavCartButton';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-scroll';
+import classes from './TheNavbar.module.css';
+import Logo from '../../assets/Logo/Logo_128_new.png';
 
 const TheNavbar = (props) => {
   //Layout and structure of the navbar to be passed to THENAV component
   return (
     <>
       <Navbar
-        expand="xl"
+        expand='xl'
         className={`${classes.navbar} fixed-top`}
-        data-aos="fade-down"
-        data-aos-easing="ease-out"
-        data-aos-duration="2000"
-      >
+        data-aos='fade-down'
+        data-aos-easing='ease-out'
+        data-aos-duration='2000'>
         <Navbar.Brand className={classes.navbar_brand}>
-          <Link to="hero" spy={true} smooth={true} offset={-50} duration={500}>
-            <img src={Logo} alt="My logo"></img>
+          <Link to='hero' spy={true} smooth={true} offset={-50} duration={500}>
+            <img src={Logo} alt='My logo'></img>
           </Link>
         </Navbar.Brand>
+
+        <Nav.Link href='#buttons' className={`${classes.m_link}`}>
+          <NavCartButton onClick={props.onShowCart} />
+        </Nav.Link>
+
         <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
+          aria-controls='basic-navbar-nav'
           className={classes.toggle}
         />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className={`${classes.nav__linkgroup} ms-auto`}>
             <Nav.Link
-              className={`${classes.nav__link} ${classes.firstnav__link} me-4`}
-            >
+              className={`${classes.nav__link} ${classes.firstnav__link} me-4`}>
               <Link
                 activeClass={classes.active}
-                to="hero"
+                to='hero'
                 spy={true}
                 smooth={true}
                 offset={-50}
-                duration={500}
-              >
+                duration={500}>
                 Home
               </Link>
             </Nav.Link>
             <Nav.Link className={`${classes.nav__link} me-4`}>
               <Link
                 activeClass={classes.active}
-                to="why"
+                to='why'
                 spy={true}
                 smooth={true}
                 offset={-50}
-                duration={500}
-              >
+                duration={500}>
                 Why choose us
               </Link>
             </Nav.Link>
             <Nav.Link className={`${classes.nav__link} me-4`}>
               <Link
                 activeClass={classes.active}
-                to="dishes"
+                to='dishes'
                 spy={true}
                 smooth={true}
                 offset={-50}
-                duration={500}
-              >
+                duration={500}>
                 Our dishes
               </Link>
             </Nav.Link>
             <Nav.Link className={`${classes.nav__link} me-4`}>
               <Link
                 activeClass={classes.active}
-                to="about"
+                to='about'
                 spy={true}
                 smooth={true}
                 offset={-50}
-                duration={500}
-              >
+                duration={500}>
                 About us
               </Link>
             </Nav.Link>
             <Nav.Link className={`${classes.nav__link} me-4`}>
               <Link
                 activeClass={classes.active}
-                to="testimonials"
+                to='testimonials'
                 spy={true}
                 smooth={true}
                 offset={-50}
-                duration={500}
-              >
+                duration={500}>
                 Testimonials
               </Link>
             </Nav.Link>
-            <Nav.Link href="#buttons" className={`${classes.nav__link}`}>
+            <Nav.Link href='#buttons' className={`${classes.nav__link}`}>
               <NavCartButton onClick={props.onShowCart} />
             </Nav.Link>
           </Nav>
